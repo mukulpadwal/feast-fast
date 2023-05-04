722b3103d5fb062ff256d084b3bf749dcf7eac47
mukulpadwal <mukulpadwal786@gmail.com>
2023-05-04 22:31:36 +0530

    Add to Cart

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import Home from './screens/Home';
 import {
   Route,
-  BrowserRouter as Router, 
+  BrowserRouter as Router,
   Routes
 } from "react-router-dom";
 import { Login } from './screens/Login';
@@ -10,18 +10,21 @@ import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import SignUp from './screens/SignUp';
+import { CartProvider } from './Context/ContextReducer';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/createuser" element={<SignUp />} />
-        </Routes>
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/createuser" element={<SignUp />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
